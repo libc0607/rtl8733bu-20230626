4a9769597358d5fc44aadfe5a69561b0fc53f48a
libc0607 <libc0607@gmail.com>
2024-05-15 17:38:00 +0800

    Add antenna noise report in radiotap

--- core/monitor/rtw_radiotap.c
+++ core/monitor/rtw_radiotap.c
@@ -163,7 +163,7 @@ static inline void _rtw_radiotap_fill_flags(struct rx_pkt_attrib *a, u8 *flags)
 
 sint rtw_fill_radiotap_hdr(_adapter *padapter, struct rx_pkt_attrib *a, u8 *buf)
 {
-#define RTAP_HDR_MAX 64
+#define RTAP_HDR_MAX 256
 
 	sint ret = _SUCCESS;
 	struct moinfo *moif = (struct moinfo *)&a->moif;
@@ -198,13 +198,14 @@ sint rtw_fill_radiotap_hdr(_adapter *padapter, struct rx_pkt_attrib *a, u8 *buf)
 
 	/* each antenna information */
 	rx_cnt = rf_type_to_rf_rx_cnt(pHalData->rf_type);
-#if 0
+#if 1
 	if (rx_cnt > 1) {
 		rtap_hdr->it_present |= BIT(IEEE80211_RADIOTAP_RADIOTAP_NAMESPACE) |
 		BIT(IEEE80211_RADIOTAP_EXT);
 
 		for (i = 1; i < rx_cnt; i++) {
 			tmp_32bit = (BIT(IEEE80211_RADIOTAP_DBM_ANTSIGNAL) |
+			             BIT(IEEE80211_RADIOTAP_DBM_ANTNOISE) |
 				     BIT(IEEE80211_RADIOTAP_LOCK_QUALITY) |
 				     BIT(IEEE80211_RADIOTAP_ANTENNA) |
 				     BIT(IEEE80211_RADIOTAP_RADIOTAP_NAMESPACE) |
@@ -214,6 +215,7 @@ sint rtw_fill_radiotap_hdr(_adapter *padapter, struct rx_pkt_attrib *a, u8 *buf)
 		}
 
 		tmp_32bit = (BIT(IEEE80211_RADIOTAP_DBM_ANTSIGNAL) |
+		             BIT(IEEE80211_RADIOTAP_DBM_ANTNOISE) |
 			     BIT(IEEE80211_RADIOTAP_LOCK_QUALITY) |
 			     BIT(IEEE80211_RADIOTAP_ANTENNA));
 		_rtw_memcpy(&hdr_buf[rt_len], &tmp_32bit, 4);
@@ -556,12 +558,16 @@ sint rtw_fill_radiotap_hdr(_adapter *padapter, struct rx_pkt_attrib *a, u8 *buf)
 	}
 
 	/* each antenna information */
-#if 0
+#if 1
 	if (rx_cnt > 1) {
 		for (i = 0; i <= rx_cnt; i++) {
 			/* dBm Antenna Signal */
-			hdr_buf[rt_len] = a->phy_info.rx_mimo_signal_strength[i];
+			hdr_buf[rt_len] = a->phy_info.rx_pwr[i];
 			rt_len += 1;
+			
+			 /*  IEEE80211_RADIOTAP_DBM_ANTNOISE */
+                        hdr_buf[rt_len] = a->phy_info.rx_pwr[i] - a->phy_info.rx_snr[i];
+                        rt_len += 1;
 
 			/* Signal Quality */
 			if (!IS_ALIGNED(rt_len, 2))
