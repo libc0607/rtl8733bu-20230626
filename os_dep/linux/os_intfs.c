9cd74b4e979eff91a84b28534e02f74bea7c9503
libc0607 <libc0607@gmail.com>
2024-05-15 17:25:00 +0800

    Fix VFS namespace issue

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -21,6 +21,9 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Realtek Wireless Lan Driver");
 MODULE_AUTHOR("Realtek Semiconductor Corp.");
 MODULE_VERSION(DRIVERVERSION);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
 
 /* module param defaults */
 int rtw_chip_version = 0x00;
