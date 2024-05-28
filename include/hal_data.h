93ce6797ea810cbd8b15495ca37be4bd63b572cd
libc0607 <332559105@qq.com>
2024-05-28 22:52:26 +0800

    Save manually tuned temperature offset here

--- include/hal_data.h
+++ include/hal_data.h
@@ -453,6 +453,7 @@ typedef struct hal_com_data {
 	u8	EEPROMVersion;
 	u8	EEPROMRegulatory;
 	u8	eeprom_thermal_meter;
+	u8 	eeprom_thermal_offset_temperature;
 	u8	EEPROMBluetoothCoexist;
 	u8	EEPROMBluetoothType;
 	u8	EEPROMBluetoothAntNum;
